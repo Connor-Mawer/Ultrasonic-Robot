9a1488285d5ccfad91feef26da3b2c53647769e6
Connor-Mawer <connormawer@gmail.com>
2021-06-26 21:14:54 +1000

    just updating

--- lib/Motors/MotorPins.h
+++ lib/Motors/MotorPins.h
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 
 
-#define PIN_MOTOR_LEFT_A -1
-#define PIN_MOTOR_LEFT_B -1
-#define PIN_MOTOR_RIGHT_A -1
-#define PIN_MOTOR_RIGHT_B -1
+#define PIN_MOTOR_LEFT_A A2
+#define PIN_MOTOR_LEFT_B A0
+#define PIN_MOTOR_RIGHT_A 7
+#define PIN_MOTOR_RIGHT_B -9
