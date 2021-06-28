01ea1c4f7f4a7b5fe8945ea3a441532bdd9566fb
Connor-Mawer <connormawer@gmail.com>
2021-06-28 14:05:55 +1000

    condensing the edetection to Ranger.cpp

--- lib/Ranger/Distance.cpp
+++ lib/Ranger/Distance.cpp
@@ -1,53 +1,56 @@
-#include <Arduino.h>
-#include <Ranger.h>
+// #include <Arduino.h>
+// #include <Ranger.h>
+// #include <Servo.h>
 
-long duration; // variable for the duration of sound wave travel
-int distance; // variable for the distance measurement
-int FDistance;
-int LDistance;
-int RDistance;
+// Servo myservo;
 
-void setup() 
-  {
-  pinMode(trigPin, OUTPUT); 
-  pinMode(echoPin, INPUT); 
-  Serial.begin(9600); 
-  }
+// long duration; // variable for the duration of sound wave travel
+// int distance; // variable for the distance measurement
+// int FDistance;
+// int LDistance;
+// int RDistance;
 
-void Distance() 
-  {
+// void setup() 
+//   {
+//   pinMode(trigPin, OUTPUT); 
+//   pinMode(echoPin, INPUT); 
+//   Serial.begin(9600); 
+//   }
 
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(2);
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
-    duration = pulseIn(echoPin, HIGH);
-    distance = duration * 0.34 / 2; //Ive converted it to mm by adjusting 0.034 x 10 to 0.34
-  }
+// void Distance() 
+//   {
 
-void Test_FDistance()
-  {
-    myservo.write(90); // This is an angle 
-    delayMicroseconds(30000);
-    Distance();
-    distance = FDistance;
-  }
+//     digitalWrite(trigPin, LOW);
+//     delayMicroseconds(2);
+//     digitalWrite(trigPin, HIGH);
+//     delayMicroseconds(10);
+//     digitalWrite(trigPin, LOW);
+//     duration = pulseIn(echoPin, HIGH);
+//     distance = duration * 0.34 / 2; //Ive converted it to mm by adjusting 0.034 to 0.34
+//   }
 
-
-void Test_RDistance()
-  {
-    myservo.write(177);
-    delayMicroseconds(30000);
-    Distance();
-    distance = RDistance;
-  }
+// void Test_FDistance()
+//   {
+//     myservo.write(90); // This is an angle 
+//     delayMicroseconds(30000);
+//     Distance();
+//     distance = FDistance;
+//   }
 
 
-void Test_LDistance()
-  {
-    myservo.write(5); // This is an angle 
-    delayMicroseconds(30000);
-    Distance();
-    distance = LDistance;
-  }
+// void Test_RDistance()
+//   {
+//     myservo.write(177);
+//     delayMicroseconds(30000);
+//     Distance();
+//     distance = RDistance;
+//   }
+
+
+// void Test_LDistance()
+//   {
+//     myservo.write(5); // This is an angle 
+//     delayMicroseconds(30000);
+//     Distance();
+//     distance = LDistance;
+//   }
--- lib/Ranger/Ranger.cpp
+++ lib/Ranger/Ranger.cpp
@@ -1,54 +1,77 @@
 #include <Ranger.h>
 #include <Servo.h>
 #include <Arduino.h>
-#include <Distnace.cpp>
-
 
 Servo myservo;
 
-void setup()
-{
-    long duration;
-    int distance;
-    myservo.attach(5);
-} 
+
+long duration; // variable for the duration of sound wave travel
+int distance; // variable for the distance measurement
+int FDistance;
+int LDistance;
+int RDistance;
+
+void setup() 
+  {
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT); 
+  Serial.begin(9600); 
+  myservo.attach(5);
+  }
+
+void Distance() 
+  {
+
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    duration = pulseIn(echoPin, HIGH);
+    distance = duration * 0.34 / 2; //Ive converted it to mm by adjusting 0.034 to 0.34
+  }
+
+void Test_FDistance()
+  {
+    myservo.write(90); // This is an angle 
+    delayMicroseconds(30000);
+    Distance();
+    distance = FDistance;
+  }
+
+
+void Test_RDistance()
+  {
+    myservo.write(177);
+    delayMicroseconds(30000);
+    Distance();
+    distance = RDistance;
+  }
+
+
+void Test_LDistance()
+  {
+    myservo.write(5); // This is an angle 
+    delayMicroseconds(30000);
+    Distance();
+    distance = LDistance;
+  }
 
 void Test_Turning_Distance()
 {
-    myservo.write(90); // This is an angle 
-    Distance();
+    Test_FDistance();
 }
 
 void Test_Moving_Distance()
 {
+    Test_FDistance();
 
-    void Test_FDistance()
-    {
-        myservo.write(90); // This is an angle 
-        delayMicroseconds(30000);
-        Distance();
-    }
+    Test_RDistance();
 
-    void Test_RDistance()
-    {
-        myservo.write(177);
-        delayMicroseconds(30000);
-        Distance();
-    }
+    Test_FDistnace();
 
-    void Test_FDistance()
-    {
-        myservo.write(90); 
-        delayMicroseconds(30000);
-        Distance();
-    }
+    Test_LDistnace();
 
-    void Test_LDistance()
-    {
-        myservo.write(5); // This is an angle 
-        delayMicroseconds(30000);
-        Distance();
-    }
 }
 
 
